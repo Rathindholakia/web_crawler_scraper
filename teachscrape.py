6279374ad0c2d7f9602615b7a121b547f773151d
Rathin Dholakia <rathindholakia@gmail.com>
2019-06-23 21:16:44 +0530

    Added whole page scraping Updated to automated paging for all pages Added Field name striping Complited file write function

--- teachscrape.py
+++ teachscrape.py
@@ -12,39 +12,63 @@ from selenium import webdriver
 import time
 import csv
 
-fd = open("contacts","+w")
+fd = open("contacts","w+")
 writer = csv.writer(fd)
-writer.writerow['Name','Designation','ResAddress','Contact','Email']
+writer.writerow(['Name','Designation','ResAddress','Contact','Email'])
 
 chrome_path = "/usr/bin/chromedriver"
 driver = webdriver.Chrome(chrome_path)
 
+pages=[14]
+
 #======= Open Site =========
 driver.get("http://www.associationofchemistryteachers.org/MembersDirectories.aspx")
-contact =[] 
 
 #------ Go to Alphabet ----------
 driver.find_element_by_xpath('//*[@id="lbtnA"]').click()
-name_id = "dListItems_ctl%02d_lblFullName"%(00)
-name= driver.find_element_by_id(name_id)
-print(name.text)
-contact.append(name.text)
-desig= driver.find_element_by_id("dListItems_ctl00_lblDesignation")
-print(desig.text)
-contact.append(desig.text)
-resAdd= driver.find_element_by_id("dListItems_ctl00_lblResAddress")
-print(resAdd.text)
-con= driver.find_element_by_id("dListItems_ctl00_lblContact")
-print(review.text)
-review= driver.find_element_by_id("dListItems_ctl00_lblEmail")
-#print(type(review))
-print(review.text)
-#for post in review:
-#    print(post.text)
-print(contact)
+for j in range(0,pages[0]):
+    if j != 0:
+        page_id = '//*[@id="dlPaging_ctl%02d_lnkbtnPaging"]'%(j)
+        driver.find_element_by_xpath(page_id).click()
+        
+    for i in range(0,12):
+        entry = [] 
+        name_id =   "dListItems_ctl%02d_lblFullName"%(i)
+        desg_id =   "dListItems_ctl%02d_lblDesignation"%(i)
+        resAdd_id = "dListItems_ctl%02d_lblResAddress"%(i)
+        con_id =    "dListItems_ctl%02d_lblContact"%(i)
+        email_id =  "dListItems_ctl%02d_lblEmail"%(i)
+        
+        try:
+            name= driver.find_element_by_id(name_id)
+            print(name.text)
+            entry.append(name.text)
+        except:
+            break
+        
 
-#---Write to file----
+        desg= driver.find_element_by_id(desg_id)
+        print(desg.text)
+        entry.append(desg.text)
 
+        resAdd= driver.find_element_by_id(resAdd_id)
+        print(resAdd.text)
+        entry.append(resAdd.text)
+
+        con= driver.find_element_by_id(con_id)
+        conE = con.text[9:]                         #Strip out field name "Contact: ""
+        print(conE)
+        entry.append(conE)
+
+        email= driver.find_element_by_id(email_id)
+        emailE = email.text[7:]                     #stip out "email: field name"              
+        print(emailE)
+        entry.append(emailE)
+
+        print(entry)
+
+        #---Write to file----
+        writer.writerow(entry)
 
 #-------- Change Page -----------------
 driver.find_element_by_xpath('//*[@id="dlPaging_ctl01_lnkbtnPaging"]').click()
